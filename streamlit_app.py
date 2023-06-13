5a625612abba6d8a7e0aeea1fcc539f919839e90
lolasanchez19 <90641849+lolasanchez19@users.noreply.github.com>
2023-06-13 15:52:21 +0200

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title ('My parents New Healty List')
+streamlit.header ('Breakfast Menu')
+streamlit.text ('Omega 3 & Blueberry Oatmeal')
+streamlit.text ('Hard-Boiled Free-Range Egg')
