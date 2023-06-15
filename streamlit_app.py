116d4e364d6eda130e2cdd4ce2700ad1f6f64bb1
lolasanchez19 <90641849+lolasanchez19@users.noreply.github.com>
2023-06-15 14:35:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,10 +31,12 @@ streamlit.dataframe(fruits_to_show)
 #NUEVA SECCION PARA LA APP
 
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import request
 
-fruityvice_response = request.get( "https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = request.get( "https://fruityvice.com/api/fruit/+fruit_choice")
 streamlit.text(fruityvice_response)
                                                                                           
 # SACAMOS EL JSON
