526a9964c3baf712e1a51f8d08f94fd3794d369b
lolasanchez19 <90641849+lolasanchez19@users.noreply.github.com>
2023-06-21 17:44:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,19 @@ import snowflake.connector
 
 streamlit.title ('My parents New Healty List')
 
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spincah & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avcado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-streamlit.header('🍌🥭 Crea tu propio batido de frutas 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
-my_fruit_list = pandas.read_csv ("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index ('Fruit')
+
+
   
 #Vamos a hacer una pick list para que puedan elegir la fruta que quieren elegir
 streamlit.multiselect ("Pick some fruits:", list(my_fruit_list.index))
@@ -28,93 +35,55 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #Mostramos la tabla en la pagina
 streamlit.dataframe(fruits_to_show)  
 
+def get_fruityvice_data(this_fruit_choce):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized 
 
 #NUEVA SECCION PARA LA APP
 
 streamlit.header("Fruityvice Fruit Advice!")
 
 
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+except URLError as e:
+  streamlit.error()
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not fruit_choice:
-  streamlit.error("Please select a fruit to get information.")
-  else 
-  fruityvice_response = request.get( "https://fruityvice.com/api/fruit/+fruit_choice" + fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  streamlit.text(fruityvice_response)
+streamlit.header("The fruit load list contains:")
 
-except URLERROR as e:
-streamlit.error()
-                                                                                          
-# SACAMOS EL JSON
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+    return my_cur.fetchall()
+streamlit.title('View Our Fruit List - Add Your Favourite')
 
-# ENSEÑAMOS EL DATAFRAME
-streamlit.dataframe(fruityvice_normalized)
+if streamlit.button ('Get Fruit list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  my_cnx.close()
+  streamlit.dataframe(my_data_rows)
 
+
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values (' " + new_fruit +" ')")
+    return "Thanks for adding "+ new_fruit
+
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
 
 
 streamlit.stop()
 
 
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_row = my_cur.fetchall()
-streamlit.header("The fruit load list contains: ")
-streamlit.dataframe(my_data_row)
-
-
-
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-                                                                                          
-                                                                                          
-  import streamlit
-import snowflake.connector
-import pandas
-
-streamlit.title("Zenas's Amazing Athleisure Catalog")          
-
-streamlit.title('Zena\'s Amazing Athleisure Catalog')
-
-# connect to snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-
-# run a snowflake query and put it all in a var called my_catalog
-my_cur.execute("select color_or_style from catalog_for_website")
-my_catalog = my_cur.fetchall()
-
-# put the dafta into a dataframe
-df = pandas.DataFrame(my_catalog)
-
-# temp write the dataframe to the page so I Can see what I am working with
-# streamlit.write(df)
-# put the first column into a list
-color_list = df[0].values.tolist()
-
-# print(color_list)
-# Let's put a pick list here so they can pick the color
-option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
-
-# We'll build the image caption now, since we can
-product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
-
-# use the option selected to go back and get all the info from the database
-my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
-df2 = my_cur.fetchone()
-streamlit.image(
-  df2[0],
-  width=400,
-  caption= product_caption
-)
-streamlit.write('Price: ', df2[1])
-streamlit.write('Sizes Available: ',df2[2])
-streamlit.write(df2[3])                                                                                        
-                                                                                          
-                                                                                          
-                                                                                          
-
-                                                                                          
-                                                                                          
-                                                                                          
+streamlit.write()                                                               
