f0c4422108cd85ac3ce79dfbe11e73a74cc66557
lolasanchez19 <90641849+lolasanchez19@users.noreply.github.com>
2023-06-21 15:26:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,10 @@ import streamlit
 import pandas
 import request
 import snowflake.connector
-from urllib.error import URLError
 
 
 streamlit.title ('My parents New Healty List')
-streamlit.header ('Breakfast Menu')
-streamlit.text ('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text ('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text ('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text(' 🥑🍞 Avocado Toast')
+
 
 streamlit.header('🍌🥭 Crea tu propio batido de frutas 🥝🍇')
 
@@ -74,7 +69,48 @@ streamlit.dataframe(my_data_row)
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
                                                                                           
                                                                                           
-                                                                                          
+  import streamlit
+import snowflake.connector
+import pandas
+
+streamlit.title("Zenas's Amazing Athleisure Catalog")          
+
+streamlit.title('Zena\'s Amazing Athleisure Catalog')
+
+# connect to snowflake
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+
+# run a snowflake query and put it all in a var called my_catalog
+my_cur.execute("select color_or_style from catalog_for_website")
+my_catalog = my_cur.fetchall()
+
+# put the dafta into a dataframe
+df = pandas.DataFrame(my_catalog)
+
+# temp write the dataframe to the page so I Can see what I am working with
+# streamlit.write(df)
+# put the first column into a list
+color_list = df[0].values.tolist()
+
+# print(color_list)
+# Let's put a pick list here so they can pick the color
+option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
+
+# We'll build the image caption now, since we can
+product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
+
+# use the option selected to go back and get all the info from the database
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
+df2 = my_cur.fetchone()
+streamlit.image(
+  df2[0],
+  width=400,
+  caption= product_caption
+)
+streamlit.write('Price: ', df2[1])
+streamlit.write('Sizes Available: ',df2[2])
+streamlit.write(df2[3])                                                                                        
                                                                                           
                                                                                           
                                                                                           
